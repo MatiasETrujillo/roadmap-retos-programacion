b288a830def5a87e74fec3976da20c842d6cd173
Matias <matiastrujillo98@hotmail.com>
2024-06-15 19:35:12 -0300

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/MatiasETrujillo.js
@@ -0,0 +1,26 @@
+// Pag con la documentación de JavaScript
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario en una linea.
+/*
+    Comentario en varias 
+    lineas de codigo
+*/
+
+// Declaración de variables
+
+var primerVariable = 'Primer Variable';
+let segundaVariable = 'Segunda Variable';
+const primeraConstante = 'Primera Constante';
+
+// Declaración de tipos de datos
+
+let string = 'Es una cadena de texto'; //string
+let numero = 1;// número puede ser entero o flotante
+let boolean = true; //booleano puede ser true o false
+let valorNulo = null; // valor nulo
+let variableVacia = ""; // undefined valor sin declarar
+let bigInt = 14654856342314584 //número entero con precisión arbitraria
+let symbol = Symbol()
+
+console.log('Hola JavaScritp')
